9263b419a4a840098ee11122d1340d10ae7c183d
chonasan <tk24120824@gmail.com>
2021-02-25 07:34:46 +0000

    コメント

---
+++ lib/object.rb
@@ -0,0 +1,26 @@
+#メソッドの呼び出し
+#誰が何をする、させる
+
+#オブジェクト.メソッド
+#オブジェクト.メソッド（引数１,引数２,引数３）
+#オブジェクト.メソッド 引数１,引数２,引数３
+
+#to_sメソッド
+
+#文字列
+puts '1'.to_s
+
+#数値
+puts 1.to_s
+
+#nill
+puts nil.to_s
+
+#true
+puts true.to_s
+
+#false
+puts false.to_s
+
+#正規表現
+puts /\d+/.to_s
---
+++ lib/string.rb
@@ -0,0 +1,7 @@
+puts'abc'.class
+
+
+puts %q!He said, "Don't speak."!
+
+something ="Hello."
+puts %!He said, "#{something}."!
